f58d0a143739c5b25672759150278b26af6b8af6
ULookup <ulookup@outlook.com>
2025-10-04 17:19:19 +0800

    Linux_Vim 2025-10-4

---
+++ Test_vim_centos/test_vim.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+  cout << "Hello,CentOS!" << endl;
+  cout << "Hello,CentOS!" << endl;
+  cout << "Hello,CentOS!" << endl;
+  cout << "Hello,CentOS!" << endl;
+  cout << "Hello,CentOS!" << endl;
+  return 0;
+}
